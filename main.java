90871899811e989fdc0dd74317cb32d3ce483f62
rafa11512 <105756020+rafa11512@users.noreply.github.com>
2025-05-04 21:30:27 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package examentapu1;
+
+import javax.swing.JFrame;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Ejercicio1 marcoSeleccionMultiple = new Ejercicio1();
+        marcoSeleccionMultiple.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoSeleccionMultiple.setSize(450, 500);
+        marcoSeleccionMultiple.setVisible(true);
+
+    }
+}
